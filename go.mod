d4f259597ba3d745aa6f4d21a7faf188193f14d0
kuritka <kuritka@gmail.com>
2020-09-23 00:10:50 +0200

    reading configs

--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.15
 
 require (
 	github.com/enescakir/emoji v1.0.0
-	//github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
-	//github.com/hashicorp/go-multierror v1.1.0
-	k8s.io/cli-runtime v0.17.2
-
 	github.com/logrusorgru/aurora v2.0.3+incompatible
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v2 v2.3.0
+	//github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
+	//github.com/hashicorp/go-multierror v1.1.0
+	k8s.io/cli-runtime v0.17.2
+	k8s.io/client-go v0.17.2
 )
--- internal/cmd/status/status.go
+++ internal/cmd/status/status.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/kuritka/k8gb-tools/internal/cmd/internal/config"
 	"github.com/kuritka/k8gb-tools/internal/cmd/internal/k8sctx"
+	"github.com/kuritka/k8gb-tools/pkg/common/guard"
 )
 
 type Status struct {
@@ -12,8 +13,17 @@ type Status struct {
 
 func New(cfg config.Config) (status *Status) {
 	status = new(Status)
-	ctx,err := k8sctx.NewContextFactory(cfg).Get()
-
+	//ctx,err := k8sctx.NewContextFactory(cfg).Get()
+	cfg, err := config.GetConfig(`/Users/ab011th/go/src/github.com/kuritka/k8gb-tools/tests/config.yaml`,"")
+	guard.FailOnError(err,"reading configuration")
+	load := make([]*k8sctx.KubeConfig,len(cfg.K8gbTools.ConfigPaths))
+	for _, path := range cfg.K8gbTools.ConfigPaths {
+		c, err := k8sctx.Get(path)
+		guard.FailOnError(err,"fail on reading %s",path)
+		load = append(load, c)
+	}
+//	ctx,_ := k8sctx.NewContextFactory(cfg).Get()
+	fmt.Println(ctx)
 	return
 }
 
